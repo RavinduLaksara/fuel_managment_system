52a0aa74941b6749569404f4c30ab090d7f2b0a1
RavinduLaksara <laksararavindum@gmail.com>
2025-01-06 20:33:09 +0530

    start a project

---
+++ src/main/java/com/example/fuel_managment_system/FuelManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.fuel_managment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelManagmentSystemApplication.class, args);
+	}
+
+}
