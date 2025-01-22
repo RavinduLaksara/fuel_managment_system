aef96911e51bfa769308a37965cf4473d32a2f1d
Ovindu17 <124423898+ovindu17@users.noreply.github.com>
2025-01-22 21:12:20 +0530

    distributions

--- src/main/java/com/example/fuel_managment_system/repository/AdminRepository.java
+++ src/main/java/com/example/fuel_managment_system/repository/AdminRepository.java
@@ -1,7 +1,7 @@
 // src/main/java/com/example/fuel_b/repository/AdminRepository.java
 package com.example.fuel_managment_system.repository;
 
-import com.example.fuel_b.entity.Admin;
+import com.example.fuel_managment_system.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/example/fuel_managment_system/repository/DMTRepository.java
+++ src/main/java/com/example/fuel_managment_system/repository/DMTRepository.java
@@ -1,7 +1,7 @@
 // src/main/java/com/example/fuel_b/repository/DMTRepository.java
 package com.example.fuel_managment_system.repository;
 
-import com.example.fuel_b.entity.DMT;
+import com.example.fuel_managment_system.entity.DMT;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
